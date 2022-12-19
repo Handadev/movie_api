d1f22c4b30ac4f48669d576ba0eeb2512825d99b
handa <akdnpdl0127@gmail.com>
2022-12-20 02:53:27 +0900

    1. java net httpRequest 테스트 작성

--- src/test/java/com/movie_api/MovieApiApplicationTests.java
+++ src/test/java/com/movie_api/MovieApiApplicationTests.java
@@ -9,6 +9,10 @@ import org.springframework.context.annotation.Bean;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.MessageDigest;
 import java.util.Base64;
 
@@ -28,6 +32,21 @@ class MovieApiApplicationTests {
         String dec = crypto.decodeAES256(enc);
 
         System.out.println("dec = " + dec);
+
+    }
+
+    @Test
+    void httpRequest() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://127.0.0.1:18080/api/v1/device/board/list?page=1&targetId=board"))
+                .GET()
+                .build();
+
+        HttpResponse<String> send = HttpClient.newBuilder()
+                .build().
+                send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("send = " + send);
 
     }
 
